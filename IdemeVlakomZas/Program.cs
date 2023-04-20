b8b48e64a5052ff27b3340370a8520a33a44d4d9
Alex Krempaský <alex.krempasky@student.tuke.sk>
2023-04-21 01:20:21 +0200

    998

--- IdemeVlakomZas/Program.cs
+++ IdemeVlakomZas/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddDbContext<ApplicationDbContent>(options => 
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -21,6 +22,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseHttpsRedirection();
 
 app.MapControllerRoute(
     name: "default",
